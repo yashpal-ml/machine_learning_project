cf085490a27f4a91140ab3c43692e4426baae76a
yash2learn <y.sharma.nagarajarao@gmail.com>
2023-01-08 11:56:19 +0530

    made minor changes to requirements.txt

--- setup.py
+++ setup.py
@@ -13,9 +13,7 @@ HYPHEN_E_DOT = "-e ."
 def get_requirements_list() -> List[str]:
     """
     Description: This function is going to return list of requirement
-    mention in requirements.txt file
-    return This function is going to return a list which contain name
-    of libraries mentioned in requirements.txt file
+    mentioned in requirements.txt file
     """
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
         requirement_list = requirement_file.readlines()
